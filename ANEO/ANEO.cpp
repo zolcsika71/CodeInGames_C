e089a9660d38876f3dae5abf3fd177acda15860b
zolcsika71 <bushdoctor2008@gmail.com>
2020-03-04 16:45:41 +0100

    aneo

--- ANEO/ANEO.cpp
+++ ANEO/ANEO.cpp
@@ -3,12 +3,28 @@
 #include <vector>
 #include <algorithm>
 
+#include <cmath>
+#include "plog\log.h"
+
+
+
 using namespace std;
+
+constexpr bool test{ true };
+
+
+// test data
+constexpr int maxSpeedTest{ 90 };
+constexpr int lightCountTest{ 3 };
+
+vector<int> distanceTest{ 300, 1500, 3000 };
+vector<int> durationTest{ 10, 10,10 };
+
 
 struct TrafficLight
 {
-    int distance;
-    int duration;
+    double distance;
+    double duration;
 };
 
 using vector_type = vector<TrafficLight>;
@@ -19,15 +35,52 @@ struct GameData
     int lightCount;
 };
 
-GameData getData()
+void fillTrafficLights(vector_type& trafficLights, int lightCount, bool test)
 {
-    GameData gameData;
+    for (int i = 0; i < lightCount; ++i) {
 
-    cin >> gameData.maxSpeed;
-    cin.ignore();
+        int distance{};
+        int duration{};
 
-    cin >> gameData.lightCount;
-    cin.ignore();
+        if (test)
+        {
+            distance = distanceTest[i];
+            duration = durationTest[i];
+        }
+        else
+        {
+            cin >> distance >> duration;
+            cin.ignore();
+        }
+
+
+        TrafficLight trafficLight{ double(distance) / 1000, double(duration) / 3600 };
+        trafficLights.push_back(trafficLight);
+    }
+}
+
+GameData getData(vector_type& trafficLights, bool test)
+{   
+    
+    
+    GameData gameData{};
+
+    if (test)
+    {
+        gameData.maxSpeed = maxSpeedTest;
+        gameData.lightCount = lightCountTest;
+    }
+    else
+    {
+        cin >> gameData.maxSpeed;
+        cin.ignore();
+
+        cin >> gameData.lightCount;
+        cin.ignore();
+    }
+   
+
+    fillTrafficLights(trafficLights, gameData.lightCount, test);
 
     return gameData;
 
@@ -43,42 +96,64 @@ void testData(GameData gameData, vector_type& trafficLights)
 
 }
 
-void fillTrafficLights(vector_type& trafficLights, int lightCount)
+bool passTrafficLight(TrafficLight trafficLight, int speed, bool test)
 {
-    for (int i = 0; i < lightCount; ++i) {
+    double hoursToLight{ trafficLight.distance / speed };
+    double period{ hoursToLight / trafficLight.duration };
+    bool returnValue{ fmod(period, 2) == 0 || period <= 1 };
 
-        int distance;
-        int duration;
+    if (test)
+    {
+        PLOGD << "speed: " << speed << " distance: " << trafficLight.distance << " duration: " << trafficLight.duration;
+        PLOGD << "hourToLight " << hoursToLight << " period: " << period;
+        PLOGD << "fmod: " << fmod(period, 2.0);
+        PLOGD << "returnValue: " << returnValue;
+    }
+    
+    
+    
+    return returnValue;
+}
 
-        cin >> distance >> duration;
-        cin.ignore();
+int solve(vector_type& trafficLights, int maxSpeed, bool test)
+{
+    for (int speed = maxSpeed; speed >= 1; --speed)
+    {
+        
+        int count{ 0 };
+               
+        for (TrafficLight trafficLight : trafficLights)
+        {
+            bool pass{ passTrafficLight(trafficLight, speed, test) };
+            if (pass)
+                ++count;
+        }
+               
 
-        TrafficLight trafficLight{ distance, duration };
-        trafficLights.push_back(trafficLight);
+        if (count == trafficLights.size())
+            return speed;
+
     }
 }
 
+
+
 int main()
 {
-    GameData gameData{ getData() };
-
+    
+    
+    plog::init(plog::debug, "Logfile.txt");
+    
+    vector_type trafficLights{};
+    GameData gameData{ getData(trafficLights, test) };
+     
     int maxSpeed{ gameData.maxSpeed };
     int lightCount{ gameData.lightCount };
 
-    vector_type trafficLights{};
+    if(test)
+        testData(gameData, trafficLights);
+       
+    cout << solve(trafficLights, maxSpeed, test) << endl;
 
-    fillTrafficLights(trafficLights, lightCount);
-
-    testData(gameData, trafficLights);
-
-
-
-
-
-
-
-    // Write an action using cout. DON'T FORGET THE "<< endl"
-    // To debug: cerr << "Debug messages..." << endl;
-
-    cout << maxSpeed << endl;
+    return 0;
 }
