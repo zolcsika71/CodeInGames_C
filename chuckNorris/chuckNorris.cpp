780104c7580e9765c55a02357d6e0564814d357f
zolcsika71 <bushdoctor2008@gmail.com>
2020-03-11 00:17:06 +0100

    CK half hour

--- chuckNorris/chuckNorris.cpp
+++ chuckNorris/chuckNorris.cpp
@@ -4,8 +4,9 @@
 #include <algorithm>
 
 using namespace std;
+using vector_string = vector<std::string>;
 
-using vector_int = vector<int>;
+bool test{ true }; 
 
 string decToBinary(int n)
 {
@@ -16,7 +17,7 @@ string decToBinary(int n)
     int binaryNum[32];
 
     // counter for binary array 
-    int i = 0;
+    int i = 0; 
     while (n > 0) {
 
         // storing remainder in binary array 
@@ -29,11 +30,9 @@ string decToBinary(int n)
 
     // printing binary array in reverse order 
     for (int j = i - 1; j >= 0; j--)
-        binaries += '0' + binaryNum[j];
+        binaries += to_string(binaryNum[j]);
 
     return binaries;
-
-
 }
 
 string getBinaries(string& message)
@@ -41,80 +40,110 @@ string getBinaries(string& message)
     
     string binaries{};
     
-    for (string::size_type i = 0; i < message.size(); ++i)    
-        binaries += decToBinary((int)message[i]);    
+    for (string::size_type i = 0; i <= message.size(); ++i)    
+        binaries += decToBinary((int)message[i]);     
        
     return binaries;
 }
 
-int countDigits(string& binaries, int index)
+void sliceString(string& binaries, vector_string& slices)
 {
+    
     int count{ 1 };
-    while (binaries[index] == binaries[index + 1])
-    {
-        ++index;
-        ++count;
-    }
-
-    return count;
-}
-
-void addBits(string& encodedBin, int index, int digitsNumber, char bit)
-{
-    for (int i = index; i <= digitsNumber; ++i)
-    {
-        encodedBin[i] = bit;
-    }
-}
-
-string encode(string& binaries)
-{
     int index{ 0 };
-    string encodedBin{};
+    
+    for (string::size_type i = 0; i < binaries.size() - 1; ++i)
+    {      
+         if (binaries[i] == binaries[i + 1])
+         {                    
+            if (count == 1)
+                index = i;
+            
+            ++count;
 
+            if(i == binaries.size() - 2)
+                slices.push_back(binaries.substr(index, count));
+            
+        }
+        else
+        {           
+           slices.push_back(binaries.substr(index, count));                            
+            
+            count = 1;        
 
-    for (std::string::size_type i = 0; i < binaries.size(); i++)
-    {
-        if (binaries[i]) // 1
-        {
-            int digitsNumber{ countDigits(binaries, index) };
-            encodedBin[index] = '0';
-            encodedBin[index + 1] = ' ';
-            addBits(encodedBin, index + 2, digitsNumber, '1');
-            index += 2 + digitsNumber;
-            cerr << index;
+            if (i == binaries.size() - 2)
+                slices.push_back(binaries.substr(index, count));
+            
         }
-        else // 0
-        {
-            int digitsNumber{ countDigits(binaries, index) };
-            encodedBin[index] = '0';
-            encodedBin[index + 1] = '0';
-            encodedBin[index + 2] = ' ';
-            addBits(encodedBin, index + 2, digitsNumber, '0');
-            index += 3 + digitsNumber;
-            cerr << index;
-        }
+
     }
-
-    return encodedBin;
 }
 
+void testArray(vector_string& testArray)
+{
+    for (string element : testArray)
+        cerr << element << '\n';
+}
 
+void pushNdigits(string& results, int n)
+{
+    for (int i = 0; i < n; ++i)
+        results += '0';   
+}
 
+string pushElements(vector_string& slices)
+{
+    string results;
+
+    for (string::size_type i = 0; i < slices.size(); ++i)
+    {
+        if (slices[i][0] == '1')        
+            results += "0 ";      
+        else        
+            results += "00 ";
+
+        pushNdigits(results, slices[i].size()); 
+
+        if(i < slices.size() - 1)
+            results += ' ';
+
+    }
+
+    return results;
+}
 
 int main()
 {
-    string MESSAGE = "CC";
-    //getline(cin, MESSAGE);
+    string MESSAGE;  
+   
+    if (test)    
+        MESSAGE = '%';
+    else
+        getline(cin, MESSAGE);
 
     string binaries{ getBinaries(MESSAGE) };
-    string outPut{ encode(binaries) };
 
-    cerr << outPut;
+    if (test)
+    {
+        cerr << binaries << '\n';
+        cerr << '\n';
+    }
+       
+
+    vector_string slices;
+    sliceString(binaries, slices);
 
 
-
+    if (test)
+    {
+        testArray(slices);
+        cerr << '\n';
+    }
     
+
+    string results{ pushElements(slices) };
+
+    cout << results;
    
     return 0;
     
